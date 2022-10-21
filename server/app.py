4ac23caa3483cbd3cbb5a0fa682e4b88ced770b8
Neev Cohen <neevc.dev@gmail.com>
2022-10-21 18:27:16 +0300

    implement basic websocket server

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from typing import Dict, Callable
+from functools import wraps
 import inspect
 import math
 import asyncio
@@ -8,16 +9,25 @@ import pyautogui
 import websockets.server
 
 Command = Dict[str, str]
+EventHandler = Callable[[Command], None] | Callable[[], None]
 
 
 class Server:
     def __init__(self):
-        self.handlers: Dict[str, Callable[[Command], None] | Callable[[], None]] = {
-            'MouseMove': self.handle_mouse_move,
-            'MouseClick': self.handle_mouse_click
-        }
+        self.handlers: Dict[str, EventHandler] = {}
 
     @staticmethod
+    def handles(event_type: str):
+        def add_handler(method: Callable):
+            @wraps(method)
+            def wrapper(self, *args, **kwargs):
+                self.handlers[event_type] = method
+                method(self, *args, **kwargs)
+            return wrapper
+        return add_handler
+
+    @staticmethod
+    @handles('MouseMove')
     def handle_mouse_move(cmd: Command):
         x, y = pyautogui.position()
         new_x = 5 * math.sin(math.radians(int(cmd['direction'])))
@@ -26,12 +36,19 @@ class Server:
         pyautogui.moveTo(x + new_x, y + new_y)
 
     @staticmethod
+    @handles('MouseClick')
     def handle_mouse_click():
         pyautogui.leftClick()
+
+    @staticmethod
+    @handles('Pause')
+    def handle_pause():
+        pyautogui.press('space')
 
     async def main_handler(self, websocket: websockets.server.WebSocketServerProtocol):
         async for message in websocket:
             try:
+                print(self.handlers)
                 cmd: Command = json.loads(message)
                 handler = self.handlers.get(cmd['type'])
                 if not handler:
