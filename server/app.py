7b3d68b9e6d035000f87141e7adf3723420f92db
Neev Cohen <neevc.dev@gmail.com>
2022-10-27 00:31:07 +0300

    implement mouse movement and button clicks

--- server/app.py
+++ server/app.py
@@ -1,68 +1,55 @@
 from typing import Dict, Callable
-from functools import wraps
-import inspect
 import math
 import asyncio
 import json
 import websockets
 import pyautogui
 import websockets.server
+from websockets.exceptions import ConnectionClosedError
 
+pyautogui.FAILSAFE = False
 Command = Dict[str, str]
 EventHandler = Callable[[Command], None] | Callable[[], None]
 
 
 class Server:
     def __init__(self):
-        self.handlers: Dict[str, EventHandler] = {}
+        self.handlers: Dict[str, EventHandler] = {
+            'MouseMove': self.handle_mouse_move,
+            'MouseClick': self.handle_mouse_click,
+        }
 
     @staticmethod
-    def handles(event_type: str):
-        def add_handler(method: Callable):
-            @wraps(method)
-            def wrapper(self, *args, **kwargs):
-                self.handlers[event_type] = method
-                method(self, *args, **kwargs)
-            return wrapper
-        return add_handler
-
-    @staticmethod
-    @handles('MouseMove')
     def handle_mouse_move(cmd: Command):
         x, y = pyautogui.position()
-        new_x = 5 * math.sin(math.radians(int(cmd['direction'])))
-        new_y = -5 * math.cos(math.radians(int(cmd['direction'])))  # multiply by negative since larger y values go
+        degrees = math.radians(int(cmd['direction']) + 90)  # nipple.js x axis is horizontal meaning
+        # 0 degrees is to the right
+        new_x = 50 * math.sin(degrees)
+        new_y = 50 * math.cos(degrees)  # multiply by negative since larger y values go
         # down in pyautogui - https://pyautogui.readthedocs.io/en/latest/mouse.html?highlight=mouse
         pyautogui.moveTo(x + new_x, y + new_y)
 
     @staticmethod
-    @handles('MouseClick')
-    def handle_mouse_click():
-        pyautogui.leftClick()
-
-    @staticmethod
-    @handles('Pause')
-    def handle_pause():
-        pyautogui.press('space')
+    def handle_mouse_click(cmd: Command):
+        if cmd['side'] == 'left':
+            pyautogui.leftClick()
+            return
+        pyautogui.rightClick()
 
     async def main_handler(self, websocket: websockets.server.WebSocketServerProtocol):
-        async for message in websocket:
-            try:
-                print(self.handlers)
+        try:
+            async for message in websocket:
                 cmd: Command = json.loads(message)
                 handler = self.handlers.get(cmd['type'])
                 if not handler:
                     raise Exception(f"No handler for message type {cmd['type']}")
-                sig = inspect.signature(handler)
-                if sig.parameters:
-                    handler(cmd)
-                    return
-                handler()
-            except Exception as e:
-                print(f'{type(e).__name__}: {e}')
+                handler(cmd)
+        except Exception as e:
+            print(f'{type(e).__name__}: {e}')
 
     async def run(self):
         async with websockets.serve(self.main_handler, '', 8000):
+            print("server running on port 8000")
             await asyncio.Future()
 
 
--- web/index.js
+++ web/index.js
@@ -1,5 +1,48 @@
+if (
+  navigator.userAgent.includes("iPhone") &&
+  navigator.userAgent.includes("Safari")
+) {
+  const buttonsElement = document.getElementById("buttons");
+  buttonsElement.style.marginBottom = "10vh";
+}
+
 const options = {
   zone: document.getElementById("joystick-zone"),
   color: "rgb(14 116 144)",
 };
-nipplejs.create(options);
+
+let manager = nipplejs.create(options);
+let ws = new WebSocket("ws://10.0.0.170:8000");
+let position;
+let interval;
+
+manager
+  .on("start", () => {
+    interval = setInterval(() => {
+      ws.send(
+        JSON.stringify({
+          type: "MouseMove",
+          direction: position.angle.degree,
+        })
+      );
+    }, 100);
+  })
+  .on("move", (_, data) => {
+    position = data;
+  })
+  .on("end", () => {
+    clearInterval(interval);
+  });
+
+let handleClick = (side) => {
+  ws.send(
+    JSON.stringify({
+      type: "MouseClick",
+      side,
+    })
+  );
+};
+
+window.onbeforeunload = () => {
+  ws.close(1000);
+};
